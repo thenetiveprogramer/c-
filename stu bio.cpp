055011dc3fd888d7afa29dbaa658e92f95fdf033
thenetiveprogramer <124654407+thenetiveprogramer@users.noreply.github.com>
2023-02-07 10:55:45 +0530

    Add files via upload

---
+++ stu bio.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+class Student{
+	
+	public:
+		int id;
+		char name[500];
+		
+};
+main()
+{
+	Student s1,s2,s3,s4,s5;
+	
+	  cout<<"enter value of id = ";
+	  cin>>s1.id;
+	  cout<<"enter name = ";
+	  cin>>s1.name;
+	  cout<<"id ="<<s1.id;
+	  cout<<"name ="<<s1.name\n;
+
+
+{
+	
+      cout<<"enter value of id = ";
+	  cin>>s2.id;
+	  cout<<"enter name = ";
+	  cin>>s2.name;
+	  cout<<"id ="<<s2.id;
+	  cout<<"name ="<<s2.name;
+}
+
+{
+
+      cout<<"enter value of id = ";
+	  cin>>s3.id;
+	  cout<<"enter name = ";
+	  cin>>s3.name;
+	  cout<<"id ="<<s3.id;
+	  cout<<"name ="<<s3.name;
+}
+
+{
+
+      cout<<"enter value of id = ";
+	  cin>>s4.id;
+	  cout<<"enter name = ";
+	  cin>>s4.name;
+	  cout<<"id ="<<s4.id;
+	  cout<<"name ="<<s4.name;
+}
+
+{
+
+      cout<<"enter value of id = ";
+	  cin>>s5.id;
+	  cout<<"enter name = ";
+	  cin>>s5.name;
+	  cout<<"id ="<<s5.id;
+	  cout<<"name ="<<s5.name;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
